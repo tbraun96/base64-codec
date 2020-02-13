c46e34be554dc32913932f72f6518ba016466510
Thomas Braun <braunth@oregonstate.edu>
2020-02-13 10:07:38 -0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! A crate for encoding/decoding streams of data using the codec abstraction
+
+#![deny(
+missing_docs,
+trivial_numeric_casts,
+unused_extern_crates,
+unused_import_braces,
+variant_size_differences,
+unused_features,
+unused_results,
+warnings
+)]
+
+/// The location for the Base64Codec
+pub mod codec;
---
+++ tests/primary.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod tests {
+    use bytes::{Bytes, BytesMut, Buf, BufMut};
+    use base64_codec::codec::Base64Codec;
+    use futures_codec::{Encoder, Decoder};
+
+    #[test]
+    fn encode_decode() {
+        let original = b"Hello, World!";
+        let mut data = BytesMut::from(original.as_ref());
+        let mut codec = Base64Codec::new();
+        let mut buffer = BytesMut::new();
+
+        let data = &mut data;
+        for x in 0..1000 {
+            data.put_i32(x);
+            assert!(codec.encode(data.clone().freeze(), &mut buffer).is_ok());
+            let res: BytesMut = codec.decode(&mut buffer).unwrap().unwrap();
+            let bytes_ret = res.bytes();
+
+            debug_assert_eq!(bytes_ret, data.bytes())
+        }
+    }
+}
